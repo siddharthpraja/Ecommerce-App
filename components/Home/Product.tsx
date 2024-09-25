9726034b9ecc45f432a6283394a3ef11ad61f7f6
Siddharth <116992422+siddharthpraja@users.noreply.github.com>
2024-09-25 23:40:32 +0530

    gridfix

--- components/Home/Product.tsx
+++ components/Home/Product.tsx
@@ -23,7 +23,7 @@ export default async function Productspage() {
   return (
     <div className="py-10">
       <div className="container mx-auto">
-        <div className="grid grid-cols-1 md:grid-cols-3 lg:grid-cols-4 gap-4">
+        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-4">
           {products.map((product) => (
             <div
               key={product.id}
